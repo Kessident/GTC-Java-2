b37a9e9321ff214e475e5a485fbe9a847a187c28
windspinks <windspinks@gmail.com>
2019-09-26 16:38:06 -0400

    import

--- Mid_project/src/com/Windspinks/Main.java
+++ Mid_project/src/com/Windspinks/Main.java
@@ -1,10 +1,8 @@
 package com.Windspinks;
 
-import com.Windspinks.PowerBall.LotteryStatistics;
-
 public class Main {
 
     public static void main(String[] args) {
-        LotteryStatistics.runStats();
+        com.Windspinks.PowerBall.LotteryStatistics.runStats();
     }
 }
