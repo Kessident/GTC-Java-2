0c2f947af3c21e63fb0ad120dbbf37a41c34aa5a
windspinks <windspinks@gmail.com>
2019-10-01 17:04:11 -0400

    spinner to textField, add helpers

--- Lab_8/src/com/Windspinks/travel_expenses/ExpenseReport.java
+++ Lab_8/src/com/Windspinks/travel_expenses/ExpenseReport.java
@@ -7,7 +7,7 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.Spinner;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -32,65 +32,66 @@ public class ExpenseReport extends Application {
 
 
         Label daysLabel = new Label("Days");
-        Spinner<Integer> daysInput = new Spinner<>(0, Integer.MAX_VALUE, 0);
-        spinnerAddFocusLost(daysInput);
+        TextField daysInput = new TextField("0");
+        restrictInputToIntegers(daysInput);
+        setToZeroIfEmpty(daysInput);
         daysInput.setEditable(true);
         HBox daysBox = new HBox(4, daysLabel, daysInput);
         daysBox.setAlignment(Pos.CENTER_LEFT);
         grid.add(daysBox, 0, 1);
 
         Label airFareLabel = new Label("Cost of Airfare");
-        Spinner<Double> airfareInput = new Spinner<>(0, Double.MAX_VALUE, 0.0);
-        spinnerAddFocusLost(airfareInput);
-        airfareInput.setEditable(true);
+        TextField airfareInput = new TextField("0");
+        restrictInputToDoubles(airfareInput);
+        setToZeroIfEmpty(airfareInput);
         HBox airfareBox = new HBox(4, airFareLabel, airfareInput);
         airfareBox.setAlignment(Pos.CENTER_LEFT);
         grid.add(airfareBox, 1, 1);
 
         Label carRentalLabel = new Label("Car Rental Fees");
-        Spinner<Double> carRentalInput = new Spinner<>(0, Double.MAX_VALUE, 0.0);
-        spinnerAddFocusLost(carRentalInput);
-        carRentalInput.setEditable(true);
+        TextField carRentalInput = new TextField("0");
+        restrictInputToDoubles(carRentalInput);
+        setToZeroIfEmpty(carRentalInput);
         HBox carRentalBox = new HBox(4, carRentalLabel, carRentalInput);
         carRentalBox.setAlignment(Pos.CENTER_LEFT);
         grid.add(carRentalBox, 0, 2);
 
         Label milesDrivenLabel = new Label("Miles Driven (If private car)");
-        Spinner<Double> milesDrivenInput = new Spinner<>(0, Double.MAX_VALUE, 0.0);
-        spinnerAddFocusLost(milesDrivenInput);
-        milesDrivenInput.setEditable(true);
+        TextField milesDrivenInput = new TextField("0");
+        restrictInputToDoubles(milesDrivenInput);
+        setToZeroIfEmpty(milesDrivenInput);
         HBox milesDrivenBox = new HBox(4, milesDrivenLabel, milesDrivenInput);
         milesDrivenBox.setAlignment(Pos.CENTER_LEFT);
         grid.add(milesDrivenBox, 1, 2);
 
         Label conFeesLabel = new Label("Conference Registration Fees");
-        Spinner<Double> conFeesInput = new Spinner<>(0, Double.MAX_VALUE, 0.0);
-        spinnerAddFocusLost(conFeesInput);
-        conFeesInput.setEditable(true);
+        TextField conFeesInput = new TextField("0");
+        restrictInputToDoubles(conFeesInput);
+        setToZeroIfEmpty(conFeesInput);
         HBox conFeesBox = new HBox(4, conFeesLabel, conFeesInput);
         conFeesBox.setAlignment(Pos.CENTER_LEFT);
         grid.add(conFeesBox, 0, 3);
 
         Label parkingLabel = new Label("Parking Fees");
-        Spinner<Double> parkingInput = new Spinner<>(0, Double.MAX_VALUE, 0.0);
-        spinnerAddFocusLost(parkingInput);
-        parkingInput.setEditable(true);
+        TextField parkingInput = new TextField("0");
+        restrictInputToDoubles(parkingInput);
+        setToZeroIfEmpty(parkingInput);
         HBox parkingBox = new HBox(4, parkingLabel, parkingInput);
         parkingBox.setAlignment(Pos.CENTER_LEFT);
         grid.add(parkingBox, 1, 3);
 
         Label lodgingLabel = new Label("Lodging Charges (per night)");
-        Spinner<Double> lodgingInput = new Spinner<>(0, Double.MAX_VALUE, 0.0);
-        spinnerAddFocusLost(lodgingInput);
-        lodgingInput.setEditable(true);
+        TextField lodgingInput = new TextField("0");
+        restrictInputToDoubles(lodgingInput);
+        setToZeroIfEmpty(lodgingInput);
         HBox lodgingBox = new HBox(4, lodgingLabel, lodgingInput);
         lodgingBox.setAlignment(Pos.CENTER_LEFT);
         grid.add(lodgingBox, 0, 4);
 
         Label taxiLabel = new Label("Taxi Charges");
-        Spinner<Double> taxiInput = new Spinner<>(0, Double.MAX_VALUE, 0.0);
-        spinnerAddFocusLost(taxiInput);
-        taxiInput.setEditable(true);
+        TextField taxiInput = new TextField("0");
+        restrictInputToDoubles(taxiInput);
+        setToZeroIfEmpty(taxiInput);
         HBox taxiBox = new HBox(4, taxiLabel, taxiInput);
         taxiBox.setAlignment(Pos.CENTER_LEFT);
         grid.add(taxiBox, 1, 4);
@@ -112,14 +113,14 @@ public class ExpenseReport extends Application {
             final double LODGING_REIMBURSEMENT = 95;
             final double PRIVATE_VEHICLE_REIMBURSEMENT_PER_MILE = 0.27;
 
-            final int DAYS_TRAVELING = daysInput.getValue();
-            final double AIRFARE = airfareInput.getValue();
-            final double CAR_RENTAL = carRentalInput.getValue();
-            final double MILES_DRIVEN = milesDrivenInput.getValue();
-            final double CON_REG_FEES = conFeesInput.getValue();
-            final double PARKING_FEES = parkingInput.getValue();
-            final double LODGING_CHARGES = lodgingInput.getValue();
-            final double TAXI_CHARGES = taxiInput.getValue();
+            final int DAYS_TRAVELING = Integer.parseInt(daysInput.getText());
+            final double AIRFARE = Double.parseDouble(airfareInput.getText());
+            final double CAR_RENTAL = Double.parseDouble(carRentalInput.getText());
+            final double MILES_DRIVEN = Double.parseDouble(milesDrivenInput.getText());
+            final double CON_REG_FEES = Double.parseDouble(conFeesInput.getText());
+            final double PARKING_FEES = Double.parseDouble(parkingInput.getText());
+            final double LODGING_CHARGES = Double.parseDouble(lodgingInput.getText());
+            final double TAXI_CHARGES = Double.parseDouble(taxiInput.getText());
             final double TOTAL_EXPENSES = AIRFARE + CAR_RENTAL + CON_REG_FEES + PARKING_FEES + LODGING_CHARGES + TAXI_CHARGES;
 
             double mealReimbursement = DAYS_TRAVELING * MEAL_REIMBURSEMENT;
@@ -157,10 +158,26 @@ public class ExpenseReport extends Application {
         primaryStage.show();
     }
 
-    private void spinnerAddFocusLost(Spinner spinner) {
-        spinner.focusedProperty().addListener((observable, oldValue, newValue) -> {
-            if (!newValue) {
-                spinner.increment(0); // won't change value, but will commit editor
+    private void restrictInputToDoubles(TextField textField) {
+        textField.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue.matches("[0-9]+(\\.)?+([0-9]{1,2})?")) {
+                textField.setText(newValue.replaceAll("[^\\d.]", ""));
+            }
+        });
+    }
+
+    private void restrictInputToIntegers(TextField textField) {
+        textField.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue.matches("\\d")) {
+                textField.setText(newValue.replaceAll("[^\\d.]", ""));
+            }
+        });
+    }
+
+    private void setToZeroIfEmpty(TextField textField) {
+        textField.focusedProperty().addListener((obs, oldVal, newVal) -> {
+            if (textField.getText().isEmpty()) {
+                textField.setText("0");
             }
         });
     }
