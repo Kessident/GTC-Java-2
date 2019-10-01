aae301127262df2bcc724a9d292012c1fe3e5e85
windspinks <windspinks@gmail.com>
2019-10-01 18:21:09 -0400

    clean

--- Lab_8/src/com/Windspinks/travel_expenses/ExpenseReport.java
+++ Lab_8/src/com/Windspinks/travel_expenses/ExpenseReport.java
@@ -6,7 +6,6 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Control;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
