ff9040f20939bf660e6ea9efb590f88859c8c3b2
windspinks <windspinks@gmail.com>
2019-11-09 16:06:36 -0500

    proper cars

--- Final Project/src/com/Windspinks/ExoticMoves/Main.java
+++ Final Project/src/com/Windspinks/ExoticMoves/Main.java
@@ -1,7 +1,6 @@
 package com.Windspinks.ExoticMoves;
 
-import com.Windspinks.ExoticMoves.Model.Aston;
-import com.Windspinks.ExoticMoves.Model.Car;
+import com.Windspinks.ExoticMoves.Model.*;
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -13,13 +12,12 @@ import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
+import java.io.*;
+import java.util.ArrayList;
 
 public class Main extends Application {
     private final File INVENTORY_FILE_LOCATION = new File("src/resources/inventory.dat");
+    ArrayList<Car> inventoryList = new ArrayList<>();
 
     public static void main(String[] args) {
         launch(args);
@@ -46,31 +44,31 @@ public class Main extends Application {
     private void createInitialInventory() {
         if (!INVENTORY_FILE_LOCATION.exists()) {
             Car blackAstConv = new Aston("Black", true, 0, new File("/resources/images/BlkAstonConvertible.jpg"));
-            Car blackFerrariConv = new Aston("Black", true, 0, new File("/resources/images/BlkFerrariConvertible.jpg"));
-            Car blackLambo = new Aston("Black", false, 0, new File("/resources/images/BlkLambo.jpg"));
-            Car blackLamboConv = new Aston("Black", true, 0, new File("/resources/images/BlkLamboConvertible.jpg"));
-            Car blackMaserati = new Aston("Black", false, 0, new File("/resources/images/BlkMaserati.jpg"));
-            Car blackMcLaren = new Aston("Black", false, 0, new File("/resources/images/BlkMcLaren.jpg"));
+            Car blackFerrariConv = new Ferrari("Black", true, 0, new File("/resources/images/BlkFerrariConvertible.jpg"));
+            Car blackLambo = new Lamborghini("Black", false, 0, new File("/resources/images/BlkLambo.jpg"));
+            Car blackLamboConv = new Lamborghini("Black", true, 0, new File("/resources/images/BlkLamboConvertible.jpg"));
+            Car blackMaserati = new Maserati("Black", false, 0, new File("/resources/images/BlkMaserati.jpg"));
+            Car blackMcLaren = new McLaren("Black", false, 0, new File("/resources/images/BlkMcLaren.jpg"));
             Car blueAston = new Aston("Blue", false, 0, new File("/resources/images/BluAston.jpg"));
-            Car blueFerrariConv = new Aston("Blue", true, 0, new File("/resources/images/BluFerrariConvertible.jpg"));
-            Car blueLamboConv = new Aston("Blue", true, 0, new File("/resources/images/BluLamboConvertible.jpg"));
-            Car blueMaserati = new Aston("Blue", false, 0, new File("/resources/images/BluMaserati.jpg"));
-            Car blueMcLaren = new Aston("Blue", false, 0, new File("/resources/images/BluMcLaren.jpg"));
+            Car blueFerrariConv = new Ferrari("Blue", true, 0, new File("/resources/images/BluFerrariConvertible.jpg"));
+            Car blueLamboConv = new Lamborghini("Blue", true, 0, new File("/resources/images/BluLamboConvertible.jpg"));
+            Car blueMaserati = new Maserati("Blue", false, 0, new File("/resources/images/BluMaserati.jpg"));
+            Car blueMcLaren = new McLaren("Blue", false, 0, new File("/resources/images/BluMcLaren.jpg"));
             Car greenAston = new Aston("Green", false, 0, new File("/resources/images/GreenAston.jpg"));
-            Car greenLambo = new Aston("Green", false, 0, new File("/resources/images/GreenLambo.jpg"));
-            Car greenMcLaren = new Aston("Green", false, 0, new File("/resources/images/GreenMcLaren.jpg"));
-            Car orangeMcLarenConv = new Aston("Orange", true, 0, new File("/resources/images/OranMcLarConvertible.jpg"));
+            Car greenLambo = new Lamborghini("Green", false, 0, new File("/resources/images/GreenLambo.jpg"));
+            Car greenMcLaren = new McLaren("Green", false, 0, new File("/resources/images/GreenMcLaren.jpg"));
+            Car orangeMcLarenConv = new McLaren("Orange", true, 0, new File("/resources/images/OranMcLarConvertible.jpg"));
             Car redAstonConv = new Aston("Red", true, 0, new File("/resources/images/RedAstonConvertible.jpg"));
-            Car redFerrari = new Aston("Red", false, 0, new File("/resources/images/RedFerrari.jpg"));
-            Car redMaserati = new Aston("Red", false, 0, new File("/resources/images/RedMaserati.jpg"));
-            Car redMcLaren = new Aston("Red", false, 0, new File("/resources/images/RedMcLaren.jpg"));
-            Car whiteFerrari = new Aston("White", false, 0, new File("/resources/images/WhiteFerrari.jpg"));
-            Car whiteFerrariConv = new Aston("White", true, 0, new File("/resources/images/WhiteFerrConvertible.jpg"));
-            Car whiteLambo = new Aston("White", false, 0, new File("/resources/images/WhiteLambo.jpg"));
-            Car whiteMaseratiConv = new Aston("White", true, 0, new File("/resources/images/WhiteMaserConvertible.jpg"));
-            Car yellowFerrari = new Aston("Yellow", false, 0, new File("/resources/images/YellowFerrari.jpg"));
-            Car yellowLambo = new Aston("Yellow", false, 0, new File("/resources/images/YellowLambo.jpg"));
-            Car yellowMcLaren = new Aston("Yellow", false, 0, new File("/resources/images/YellowMcLaren.jpg"));
+            Car redFerrari = new Ferrari("Red", false, 0, new File("/resources/images/RedFerrari.jpg"));
+            Car redMaserati = new Maserati("Red", false, 0, new File("/resources/images/RedMaserati.jpg"));
+            Car redMcLaren = new McLaren("Red", false, 0, new File("/resources/images/RedMcLaren.jpg"));
+            Car whiteFerrari = new Ferrari("White", false, 0, new File("/resources/images/WhiteFerrari.jpg"));
+            Car whiteFerrariConv = new Ferrari("White", true, 0, new File("/resources/images/WhiteFerrConvertible.jpg"));
+            Car whiteLambo = new Lamborghini("White", false, 0, new File("/resources/images/WhiteLambo.jpg"));
+            Car whiteMaseratiConv = new Maserati("White", true, 0, new File("/resources/images/WhiteMaserConvertible.jpg"));
+            Car yellowFerrari = new Ferrari("Yellow", false, 0, new File("/resources/images/YellowFerrari.jpg"));
+            Car yellowLambo = new Lamborghini("Yellow", false, 0, new File("/resources/images/YellowLambo.jpg"));
+            Car yellowMcLaren = new McLaren("Yellow", false, 0, new File("/resources/images/YellowMcLaren.jpg"));
 
             try {
                 ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(INVENTORY_FILE_LOCATION));
